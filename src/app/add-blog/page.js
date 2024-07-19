c773a999dc9f41f1b16c143f65fee868d19eab4b
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-19 11:47:48 +0700

    blog data

---
+++ src/app/add-blog/page.js
@@ -0,0 +1,54 @@
+"use client";
+import { useState } from "react";
+
+const initialFormData = {
+  title: "",
+  description: "",
+};
+export default function AddBlog() {
+  const [blogFormData, setBlogFormData] = useState(initialFormData);
+
+  console.log("=====================")
+  console.log(blogFormData)
+  console.log("=====================")
+
+  return (
+    <main className="flex min-h-screen flex-col p-8">
+      <h1 className="font-bold text-lg mb-3">Add a New Blog</h1>
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-3">
+          <label>Enter blog title</label>
+          <input
+            className="border border-red-500 p-3 outline-none"
+            name="title"
+            placeholder="enter blog title"
+            value={blogFormData["title"]}
+            onChange={(e)=> setBlogFormData({
+              ...blogFormData,
+              title: e.target.value
+            })}
+          />
+        </div>
+        <div className="flex flex-col gap-3">
+          <label>Enter blog description</label>
+          <textarea
+            rows={5}
+            className="border border-red-500 p-3 outline-none"
+            name="description"
+            placeholder="enter blog description"
+            value={blogFormData["description"]}
+            onChange={(e)=> setBlogFormData({
+              ...blogFormData,
+              description: e.target.value
+            })}
+          />
+        </div>
+        <div>
+          <button className="border border-red-500 p-4 bg-black text-white">
+            Add Blog
+          </button>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ src/app/blog-list/page.js
@@ -0,0 +1,8 @@
+
+export default function BlogList() {
+  return (
+    <main className="flex min-h-screen flex-col p-8">
+      <h1 className="font-bold text-lg">Blog List</h1>
+    </main>
+  );
+}
