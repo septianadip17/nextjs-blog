dce1cabdb9f54ea812b709cab967dedf6735b479
Septian Adi Pratama <septianadip7998@gmail.com>
2024-07-20 10:01:52 +0700

    delete button added

--- src/components/blog/list/index.js
+++ src/components/blog/list/index.js
@@ -15,9 +15,10 @@ export default function BlogListComponent({ getAllBlogs }) {
     <div className="grid grid-cols-3 gap-4">
       {getAllBlogs && getAllBlogs.length > 0 ? (
         getAllBlogs.map((blogItem) => (
-          <div className="p-4 border border-red-600" key={blogItem._id}>
-            <p>{blogItem.title}</p>
+          <div className="p-4 border flex flex-col gap-4  border-red-600" key={blogItem._id}>
+            <p className="font-bold">{blogItem.title}</p>
             <p>{blogItem.description}</p>
+            <button className="border border-red-500 p-4 bg-black text-white">Delete Blog Item</button>
           </div>
         ))
       ) : (
